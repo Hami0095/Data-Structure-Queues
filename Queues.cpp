2b70ec2db82e2ba2e658c3aadeee9d298abd5346
Mohammad Abdur Rehman Cheema <59124007+Hami0095@users.noreply.github.com>
2021-03-20 15:58:13 +0500

    Update Queues.cpp

--- Queues.cpp
+++ Queues.cpp
@@ -2,10 +2,19 @@
 using namespace std;
 
 /// <summary>
-/// This queue is VECTOR BASED queue
-/// 
+/// This queue is VECTOR BASED queue with following functions and the use of function is also given :)
+/// Enqueue() -> To Enter new element at the rear of the Queue
+/// Dequeue() -> To remove the first element, i.e at Front
+/// Peek() -> To return the value at the first i.e front + 1
+/// next() -> To get the next value in queue
+/// previous() -> not used yet, but it can be used to go back to previous term in queue
+/// Empty() -> to tell wether the queue is empty or not?
+/// PrintQueue() -> just to check wether our queue is working good?
+/// doubleBox() -> to manage our queue
+/// get_Number_Of_Elements() -> to check how many elements we have in our queue?
 /// </summary>
 /// <typeparam name="T"></typeparam>
+
 template<typename T>
 class Queue {
 private:
@@ -26,18 +35,24 @@ public:
 		else
 			box = new T[cap];
 	}
-	bool Empty() {
-		return size == 0;
+
+	// No error in is_Empty() 
+	bool is_Empty() {
+		return (number_Of_Elements == 0);
 	}
+	// next is working accurately
 	int next(int i) {
 		return ((i + 1) % capacity);
 	}
+	// No error in previous()
 	int previous(int i) {
 		return ((i + (capacity - 1)) % capacity);
 	}
+	// No error in get_Number_Of_Elements()
 	int get_Number_Of_Elements() {
 		return number_Of_Elements;
 	}
+	// no error in Double Box
 	void double_Box() {
 		capacity *= 2;
 		T * temp = new T[capacity];
@@ -50,11 +65,14 @@ public:
 		delete[] box;
 		box = temp;
 	}
+	
+	// working great
 	const T & peek() {
 		T a = box[front];
 		front = next(front);
 		return a;
 	}
+	// working Ok
 	void printQueue() {
 		cout << "Front is at : " << front << endl;
 		cout << "Rear is at : " << rear << endl;
@@ -65,8 +83,10 @@ public:
 		
 		cout << "------------------------" << endl;
 	}
+	// working OK -> Enqueue && Dequeue both:
+	
 	void Enqueue(const T& data);
-	const T& Dequeue();
+	void Dequeue();
 	~Queue() {
 		delete[] box;
 	}
@@ -84,7 +104,7 @@ void Queue<T>::Enqueue(const T& data) {
 }
 
 template<class T>
-const T& Queue<T> ::Dequeue() {
-	front = previous(front);
+void Queue<T> ::Dequeue() {
+	front = next(front);
 	number_Of_Elements--;
 }
